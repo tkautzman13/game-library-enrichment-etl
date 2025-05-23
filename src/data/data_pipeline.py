c0e40deff6ccea8d1ec71a6b44697fdde3c4f11a
tkautzman13 <tkautzman13@gmail.com>
2025-05-22 21:05:54 -0700

    game_library abstraction + data_pipeline expansion - Added yaml config import to beginning of game_library.py - Removed yaml config import from data_pipeline - Abstracted game_library by using default parameters, simplifying data_pipeline - Added hltb_prepare... and extract_raw_hltb... to data_pipeline

--- src/data/data_pipeline.py
+++ src/data/data_pipeline.py
@@ -1,10 +1,13 @@
-from data.game_library import collect_library_raw_data, process_library_data
-import yaml
+from data.game_library import copy_raw_library_data, process_library_data
+from data.how_long_to_beat import hltb_prepare_library_data, extract_raw_hltb_data
 
-# Pull in yaml config file
-with open("config.yaml", "r") as f:
-    config = yaml.safe_load(f)
 
-# Collect Game Library Data
-collect_library_raw_data(input_path=config["data"]["library_input_path"], output_path=config["data"]["library_raw_path"])
-process_library_data(input_path=config["data"]["library_raw_path"], output_path=config["data"]["library_processed_path"])
+# Collect game library data
+copy_raw_library_data()
+process_library_data()
+
+# Prepare library data for HLTB data collection
+library_prepped = hltb_prepare_library_data()
+
+# Collect HLTB playtime data
+extract_raw_hltb_data(library_prepped=library_prepped)
--- src/data/game_library.py
+++ src/data/game_library.py
@@ -1,32 +1,83 @@
 import pandas as pd
+import yaml
 
-def collect_library_raw_data(input_path, output_path):
-    library_raw = pd.read_csv(input_path)
-    library_raw.to_csv(output_path, index = False)
-
-    print(f'Library data successfully pulled from {input_path} and stored in: {output_path}')
+# Pull in yaml config file
+with open("config.yaml", "r") as f:
+    config = yaml.safe_load(f)
 
 
-def process_library_data(input_path,output_path):
-    library_interm = pd.read_csv(input_path)
+def copy_raw_library_data(
+    input_file=config["data"]["library_source_file"],
+    output_file=f'{config["data"]["raw_path"]}library_raw.csv',
+):
+    """
+    Reads a CSV file containing raw library data from the specified input path,
+    and writes it to the specified output path without any modification.
+
+    Parameters:
+    -----------
+    input_file : str
+        The file path to the input CSV file containing the raw library data.
+    output_file : str
+        The file path where the raw data CSV will be saved.
+
+    Returns:
+    --------
+    None
+    """
+    library_raw = pd.read_csv(input_file)
+    library_raw.to_csv(output_file, index=False)
+
+    print(
+        f"Library data successfully pulled from {input_file} and stored in: {output_file}"
+    )
+
+
+def process_library_data(
+    input_file=f'{config["data"]["raw_path"]}library_raw.csv',
+    output_file=f'{config["data"]["interm_path"]}library_interm.csv',
+):
+    """
+    Reads a CSV file containing library data, cleans and processes the data by:
+    - Removing platform tags (e.g., '(Xbox)', '(Game Pass)', etc.) from game names.
+    - Filtering out games categorized as 'Apps'.
+    - Removing entries with missing 'Completion Status'.
+    - Dropping duplicate games based on 'Name' and 'Release Date'.
+    Finally, saves the cleaned data to the specified output path.
+
+    Parameters:
+    -----------
+    input_file : str
+        The file path to the input CSV file containing raw or intermediate library data.
+    output_file : str
+        The file path where the cleaned and processed CSV data will be saved.
+
+    Returns:
+    --------
+    None
+    """
+    library_interm = pd.read_csv(input_file)
 
     # Remove (Xbox), (Game Pass), (Switch), (PlayStation) tags from game names
-    tags = {' (Xbox)', ' (Game Pass)', ' (Switch)', ' (PlayStation)'}
-    replacement = ''
+    tags = {" (Xbox)", " (Game Pass)", " (Switch)", " (PlayStation)"}
+    replacement = ""
 
     for tag in tags:
-        library_interm['Name'] = library_interm['Name'].str.replace(tag, replacement)
+        library_interm["Name"] = library_interm["Name"].str.replace(tag, replacement)
 
-    # No 'Apps' Category games (ie GamePass)
-    library_interm[library_interm['Completion Status'] != 'Apps']
+    # Exclude 'Apps' Category games (ie GamePass)
+    library_interm[library_interm["Categories"] != "Apps"]
 
-    # No games without a library Completion Status
-    library_interm = library_interm[~library_interm['Completion Status'].isnull()]
+    # Exclude games without a library Completion Status
+    library_interm = library_interm[~library_interm["Completion Status"].isnull()]
+
+    # Exclude hidden games
+    library_interm = library_interm[library_interm["Hidden"] == False]
 
     # Drop duplicates
-    library_interm = library_interm.drop_duplicates(subset=['Name', 'Release Date'])
+    library_interm = library_interm.drop_duplicates(subset=["Name", "Release Date"])
 
     # Export intermediate data
-    library_interm.to_csv(output_path, index=False)
+    library_interm.to_csv(output_file, index=False)
 
-    print(f'Library data successfully processed and stored in: {output_path}')
+    print(f"Library data successfully processed and stored in: {output_file}")
