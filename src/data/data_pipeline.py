fc7d39abbc3d4e74baa70f07e7aaed5657be212d
tkautzman13 <tkautzman13@gmail.com>
2025-06-02 17:20:39 -0700

    Adjusted for function renaming

--- src/data/data_pipeline.py
+++ src/data/data_pipeline.py
@@ -1,16 +1,18 @@
-from data.game_library import copy_raw_library_data, clean_library_data
-from data.how_long_to_beat import prepare_library_data_hltb, extract_raw_hltb_data, process_hltb_data
+from data.game_library import extract_library_data, transform_library_data
+from data.how_long_to_beat import transform_library_data_for_hltb, extract_hltb_data, transform_hltb_data
 
+# Config file path
+config_path = 'config.yaml'
 
 # Collect game library data
-copy_raw_library_data()
-clean_library_data()
+extract_library_data()
+transform_library_data()
 
 # Prepare library data for HLTB data collection
-prepare_library_data_hltb()
+transform_library_data_for_hltb()
 
 # Collect HLTB playtime data
-extract_raw_hltb_data()
+extract_hltb_data()
 
-# Process HLTB + Library data
-process_hltb_data()
+# Process HLTB data
+transform_hltb_data()
